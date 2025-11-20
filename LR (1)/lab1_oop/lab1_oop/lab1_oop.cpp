07d8814ff465689b0758b1a3256dd40b535a98cc
Minores-univer <starmin@list.ru>
2025-11-20 15:44:16 +0300

    modified:   LR (1)/lab1_oop/lab1_oop/lab1_oop.cpp

--- LR (1)/lab1_oop/lab1_oop/lab1_oop.cpp
+++ LR (1)/lab1_oop/lab1_oop/lab1_oop.cpp
@@ -9,7 +9,7 @@ int main()
     cin >> length;
     if (length <= 0)
     {
-        cout << "Некорректное число" << endl;
+        cout << "Некорректное число1" << endl;
         exit(0);
     }
     int* mas = new int[length];
